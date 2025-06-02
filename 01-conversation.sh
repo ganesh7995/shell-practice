4c196129b447be0e062a32cda5a81b5670598a6c
ganesh7995 <gundaganes040@gmail.com>
2025-06-02 09:16:23 +0530

    talking

--- 01-conversation.sh
+++ 01-conversation.sh
@@ -3,9 +3,9 @@
 PERSON1=$1
 PERSON2=$2
 
-echo "$PERSON1:HI"
-echo "$PERSON2:Hlo,ela unnav"
-echo "$PERSON1:super nv mari"
-echo "$PERSON2:baagunna,em chestunnav"
-echo "$PERSON1:kaali kalavachu kada once"
-echo "$PERSON2:next month lo vasta"
+echo "$1:HI"
+echo "$2:Hlo,ela unnav"
+echo "$1:super nv mari"
+echo "$2:baagunna,em chestunnav"
+echo "$1:kaali kalavachu kada once"
+echo "$2:next month lo vasta"
