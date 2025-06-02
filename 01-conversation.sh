268a809b567aed7df19f687feb436e70f7441dfc
ganesh7995 <gundaganes040@gmail.com>
2025-06-02 09:12:47 +0530

    talking

---
+++ 01-conversation.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+PERSON1=$1
+PERSON2=$1
+
+echo "$PERSON1:HI"
+echo "$PERSON2:Hlo,ela unnav"
+echo "$PERSON1:super nv mari"
+echo "$PERSON2:baagunna,em chestunnav"
+echo "$PERSON1:kaali kalavachu kada once"
+echo "$PERSON2:next month lo vasta"
