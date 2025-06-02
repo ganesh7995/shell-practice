156aafabe4ea673d06a6ccf3bdc3580163651509
ganesh7995 <gundaganes040@gmail.com>
2025-06-02 20:17:12 +0530

    statement

--- 03-install.sh
+++ 03-install.sh
@@ -3,7 +3,7 @@ USERID=$(id -u)
 if [ USERID -ne 0 ]
 then
     echo -e " \e[31m ERROR:: \e[0m Please run with root access"
-    exit1
+    exit 1
 else
     echo "you are running with root access"
 fi
