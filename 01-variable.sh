f798cfb0698099caeeb42be8f91cb5f3ae8c08ef
ganesh7995 <gundaganes040@gmail.com>
2025-06-02 10:00:49 +0530

    talking

--- 01-variable.sh
+++ 01-variable.sh
@@ -2,7 +2,8 @@ echo "number of variables passed:$#"
 echo "script name:$0"
 echo "present working directory:$PWD"
 echo "which user running the script:$USER"
+echo "PID of the current script is :$$"
 
-sleep 10 &
+sleep 15 &
 
-PID odf the last command running in background:$!
+echo "PID of the last command running in background":$!
