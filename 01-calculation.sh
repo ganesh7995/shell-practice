b05eb1eb35313c7b8d458bddf10495c626f3893c
ganesh7995 <gundaganes040@gmail.com>
2025-06-02 09:08:13 +0530

    talking

---
+++ 01-calculation.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+PERSON1=ganesh
+PERSON2=chandhu
+
+echo "ganesh:HI"
+echo "chandhu:Hlo,ela unnav"
+echo "ganesh:super nv mari"
+echo "chandhu:baagunna,em chestunnav"
+echo "ganesh:kaali kalavachu kada once"
+echo "chandhu:next month lo vasta"
