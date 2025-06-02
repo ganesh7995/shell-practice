ea18a2ca421774283dc19b97529e2cea7d9d5fe9
ganesh7995 <gundaganes040@gmail.com>
2025-06-02 08:04:11 +0530

    talking

--- 02-teach.sh
+++ 02-teach.sh
@@ -1,5 +1,18 @@
 #!bin/bash
 
-echo "ganesh: Hi"
+PERSON1=ganesh
+PERSON2=Chandhu
 
-echo "Chandhu:Hlo"
+echo "$PERSON1: Hi"
+
+echo "$PERSON2:Hlo"
+
+echo "$PERSON1:atu velthav today"
+
+echo "$PERSON2:atu vellateldhu mari nv"
+
+echo "$PERSON1:movie ki vetha vastava"
+
+echo "$PERSON2:a movie ki"
+
+echo "$PERSON1:HIT"
