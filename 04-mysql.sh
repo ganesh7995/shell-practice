447040775a003100016a3b6418d09cb12bc380a2
ganesh7995 <gundaganes040@gmail.com>
2025-06-03 11:25:14 +0530

    python3

--- 04-mysql.sh
+++ 04-mysql.sh
@@ -1,8 +1,19 @@
 USERID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIP_NAME="$(echo $0 |cut -d "." -f1)"
+LOG_FILE=$(LOGS_FOLDER/SCRIP_NAME.log)
+mkdir -p $LOGS_FOLDER
+echo "script executing date:$(date)" |tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR::Please run with root access"
+    echo "ERROR::Please run with root access"|tee -a $LOG_FILE
     exit 1
 else
     echo "you are running with root access"
@@ -12,7 +23,7 @@ dnf install mysql -y
 
 if [ $? -eq 0 ]
 then
-    echo "Installing MYSQL ...SUCCESS"
+    echo "Installing MYSQL ...SUCCESS"|tee -a $LOG_FILE
 else
     echo "Installing MYSQL... FALIED"
     exit 1
