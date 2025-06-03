7dfd327de824a8af7eb784c42727c6a7d54b756f
ganesh7995 <gundaganes040@gmail.com>
2025-06-03 12:32:32 +0530

    python3

--- 04-mysql.sh
+++ 04-mysql.sh
@@ -7,13 +7,13 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIP_NAME="$(echo $0 |cut -d "." -f1)"
-LOG_FILE=$(LOGS_FOLDER/SCRIP_NAME.log)
+LOG_FILE="$LOGS_FOLDER/$SCRIP_NAME.log"
 mkdir -p $LOGS_FOLDER
 echo "script executing date:$(date)" |tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR::Please run with root access"|tee -a $LOG_FILE
+    echo -e "$R ERROR:: $N Please run with root access"|tee -a $LOG_FILE
     exit 1
 else
     echo "you are running with root access"
@@ -23,8 +23,8 @@ dnf install mysql -y
 
 if [ $? -eq 0 ]
 then
-    echo "Installing MYSQL ...SUCCESS"|tee -a $LOG_FILE
+    echo -e "Installing MYSQL ... $G SUCCESS $N"|tee -a $LOG_FILE
 else
-    echo "Installing MYSQL... FALIED"
+    echo -e "Installing MYSQL... $R FALIED $N"
     exit 1
 fi
