fd85da41d4546773b9c62f91eb02acb10cfdf3ac
ganesh7995 <gundaganes040@gmail.com>
2025-06-03 10:57:30 +0530

    python3

---
+++ 08-nginx.sh
@@ -0,0 +1,38 @@
+USERID=$( id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:run with root user"
+    exit1 1
+else
+    echo "your already root user"
+fi
+
+VALIDATION () {
+if [ $1 -eq 0 ]
+then
+    echo "installation $2 is ... SUCCESS"
+
+else
+    echo "installation $2 is ... FAILED"
+
+    exit 1
+fi
+
+}
+
+dnf list install nginx
+
+if [ $? -eq 0 ]
+
+then
+    echo "need to install nginx in your system"
+
+dnf install nginx -y
+
+VALIDATION $? "nginx"
+
+else
+    echo "nginx already installed in your system"
+    exit 1
+fi
