fad56bb988e3197786250a18409053fe1f62f56e
ganesh7995 <gundaganes040@gmail.com>
2025-06-03 13:12:51 +0530

    python3

--- 06-validation.sh
+++ 06-validation.sh
@@ -9,36 +9,36 @@ LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIP_NAME="$(echo $0 |cut -d "." -f1)"
 LOG_FILE="$LOGS_FOLDER/$SCRIP_NAME.log"
 mkdir -p $LOGS_FOLDER
-echo "script executing date:$(date)" &>>$LOG_FILE
+echo "script executing date:$(date)" |tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R ERROR $N: please run with root access" &>>$LOG_FILE
+    echo -e "$R ERROR $N: please run with root access" |tee -a $LOG_FILE
     exit 1
 else
-    echo "you are already root user" &>>$LOG_FILE
+    echo "you are already root user" |tee -a $LOG_FILE
 fi
 
 VALIDATE (){
     if [ $1 -eq 0 ]
     then 
-        echo -e "installing $2 is ..... $G SUCCESS $N" &>>$LOG_FILE
+        echo -e "installing $2 is ..... $G SUCCESS $N" |tee -a $LOG_FILE
     else
         echo -e "installing $2 is ....$R Failed $N"
     exit 1
     fi
 }
 
-dnf list install mysql &>>$LOG_FILE
+dnf list install mysql |tee -a $LOG_FILE
 
 if [ $? -ne 0 ]
 then
-    echo "need to install my sql" &>>$LOG_FILE
+    echo "need to install my sql" |tee -a $LOG_FILE
 
-dnf install mysql -y &>>$LOG_FILE
+dnf install mysql -y |tee -a $LOG_FILE
 
 VALIDATE $? "mysql" 
 
 else
-    echo -e  " $Y already mysql installed $N" &>>$LOG_FILE
+    echo -e  " $Y already mysql installed $N" |tee -a $LOG_FILE
 fi
