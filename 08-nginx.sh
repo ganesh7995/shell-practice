8f44819565622b0f7edcd1e5bea1b189c099154d
ganesh7995 <gundaganes040@gmail.com>
2025-06-03 13:33:48 +0530

    python3

--- 08-nginx.sh
+++ 08-nginx.sh
@@ -1,39 +1,51 @@
 USERID=$( id -u)
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=$LOGS_FOLDER/$SCRIP_NAME.log
+mkdir -p $LOGS_FOLDER
+
+echo "script execution time at: $(date)" |tee -a $LOG_FILE
+
 if [ $USERID -ne 0 ]
 then
-    echo "ERROR:run with root user"
+    echo -e "$R ERROR:run with root user $N" |tee -a $LOG_FILE
     exit1
 else
-    echo "your already root user"
+    echo -e "$G your already root user $N" |tee -a $LOG_FILE
 fi
 
 VALIDATION () {
 if [ $1 -eq 0 ]
 then
-    echo "installation $2 is ... SUCCESS"
+    echo -e "installation $2 is ... $G SUCCESS $N" |tee -a $LOG_FILE
     exit1
 
 else
-    echo "installation $2 is ... FAILED"
+    echo -e "installation $2 is ... $R FAILED $N" |tee -a $LOG_FILE
 
     exit1
 fi
 
 }
 
-dnf list install nginx
+dnf list installed nginx &>> $LOG_FILE
 
 if [ $? -eq 0 ]
 
 then
-    echo "need to install nginx in your system"
+    echo -e "$Y need to install nginx in your system $N"
     
-dnf install nginx -y
+dnf install nginx -y &>> $LOG_FILE
 
 VALIDATION $? "nginx"
 
 else
-    echo "nginx already installed in your system"
+    echo -e "$Y nginx already installed in your system $N"
    exit1
 fi
